04c3e2badf4fc1fddd584c61a8d23b389572e186
damilola0234 <wusuoluwadamilola01@gmail.com>
2025-10-14 09:05:57 +0100

    Prepare for Railway deployment

--- src/test/java/com/example/qrattendance/QrattendanceApplicationTests.java
+++ src/test/java/com/example/qrattendance/QrattendanceApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest
 @ActiveProfiles("test")
 class QrattendanceApplicationTests {
-	//@Test
-	//void contextLoads() {
-//	}
+	@Test
+	void contextLoads() {
+	}
 }
